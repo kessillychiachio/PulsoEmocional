e13477273242bf30d83f16075eeee8a59b8e1152
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-07-15 09:27:06 -0300

    gemini com langchain

--- inicializacao_IA.py
+++ inicializacao_IA.py
@@ -1,49 +1,57 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
+
 import os
-import google.generativeai as genai
 
-CHAVE_KEY="keys/google_ai_studio.key"
-MODELO="gemini-1.5-flash"
+CHAVE_KEY = "keys/gemini.key"
+MODELO = "gemini-2.0-flash"
 
-def iniciar_IA():
-    sucesso, modelo_ia = False, None
+def iniciar_IA(contexto = None):
+    sucesso, IA = False, None
+    
     try:
         with open(CHAVE_KEY, "r") as arquivo_chave:
             chave = arquivo_chave.read().strip()
-            genai.configure(api_key=chave) 
-            arquivo_chave.close()
-            
-            modelo_ia = genai.GenerativeModel(MODELO)
-            
-            sucesso = True
+            os.environ["GOOGLE_API_KEY"] = chave
+
+        llm = ChatGoogleGenerativeAI(model=MODELO, temperature=0, max_output_tokens=None, timeout=None, max_retries=2)
+        
+        if contexto is not None:
+            IA = ChatPromptTemplate.from_messages(contexto) | llm
+        else:
+            IA = llm
+
+        sucesso = True
+    except FileNotFoundError:
+        print(f"ocorreu um erro iniciando acesso à IA: o arquivo de chave '{CHAVE_KEY}' não foi encontrado.")
     except Exception as e:
-        print(f"Ocorreu um erro iniciando acesso a IA: {str(e)}")
+        print(f"ocorreu um erro iniciando acesso à IA: {str(e)}")
+    
+    return sucesso, IA
 
-    return sucesso, modelo_ia
-
-def obter_resposta(modelo_ia, prompt_texto):
+def obter_resposta(IA, parametros):
     sucesso, resposta = False, None
+    
     try:
-        response_obj = modelo_ia.generate_content(prompt_texto)
-        resposta = response_obj.text
+        resposta = IA.invoke(parametros)
+        
         sucesso = True
     except Exception as e:
-        print(f"Ocorreu um erro testando o prompt: {str(e)}")
-        
+        print(f"ocorreu um erro testando o prompt: {str(e)}")
+
     return sucesso, resposta
 
 if __name__ == "__main__":
-    sucesso, ia_pronta = iniciar_IA()
-
+    sucesso, IA = iniciar_IA()
     if sucesso:
-        print("Acesso à IA iniciado, iniciando o chat de teste...")
-        
-        prompt_de_teste = "Responda SIM se você consegue realizar análise de sentimentos sobre trechos de texto. Caso contrário, responda NÃO."
-        
-        sucesso, resposta = obter_resposta(ia_pronta, prompt_de_teste)
-        
+        print("acesso à IA iniciado, iniciando o chat...")
+
+        sucesso, resposta = obter_resposta(IA, [
+            SystemMessage(content="Responda SIM se você consegue realizar análise de sentimentos sobre trechos de texto. Caso contrário, responda NÃO"),
+            HumanMessage(content="Você é capaz de realizar análise de sentimentos?")
+        ])
         if sucesso:
-            print(f"Resposta: {resposta}")
+            print(f"Resposta (Análise de Sentimentos): {resposta.content}")
         else:
-            print("Não foi possível obter uma resposta para o teste de prompt.")
-    else:
-        print("Falha ao iniciar acesso à IA. Verifique as mensagens de erro acima.")
+            print("Não foi possível obter resposta para a pergunta de análise de sentimentos.")
