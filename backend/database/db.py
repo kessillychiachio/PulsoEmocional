7b88a9a052bbdfc34aa5b05f4f9fd93445752522
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-11-10 20:09:19 -0300

    Utiliza NullPool para inicialização do mecanismo SQLite. Impede o agrupamento de conexões para melhorar a compatibilidade com o SQLite em ambientes multithread. Resolve possíveis problemas com acesso simultâneo e disputa de recursos.

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 
-engine = create_engine("sqlite:///./data.db", connect_args={"check_same_thread": False})
+engine = create_engine(
+    "sqlite:///./data.db", 
+    connect_args={"check_same_thread": False}, 
+    poolclass=NullPool
+)
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
