3e1fb5712f3f32aef8764d5ed67b27cf69af2c63
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-11-13 21:50:16 -0300

    refatorando

--- backend/database/create_db.py
+++ backend/database/create_db.py
@@ -1,4 +1,3 @@
 from backend.database.db import engine, Base
-from backend.models.video_models import Video, Comentario
 
 Base.metadata.create_all(bind=engine)
--- backend/dependencies.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, Depends, Request
-
-def get_ia_model(request: Request):
-    ia_model = getattr(request.app.state, "ia_modelo", None)
-    if ia_model is None:
-        raise HTTPException(
-            status_code=500, detail="O modelo de IA não está inicializado."
-        )
-    return ia_model
